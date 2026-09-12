e4fafacfdbb53fb91fd69b01c71fdd5f1e46b039
LogixAlpha <ayoub.ouaddi.yt@gmail.com>
2026-09-12 20:36:51 +0100

    initialize git

--- [not possible ig-LINUX ONLY]scrcpy.py
+++ [not possible ig-LINUX ONLY]scrcpy.py
@@ -1,16 +1,14 @@
 import time 
 from colorama import Fore , Back , init , Style
-#import keyboard
-#import os
-# LINUX ONLY 
+import keyboard
+
+# Windows users ONLY 
 
 init(autoreset=True)
-#if os.geteuid() == 0 and "SUDO_USER" in os.environ:
-print(f"Launched via sudo by user: {os.environ['SUDO_USER']}")
+
 print(f"{Back.CYAN}How to use Scrcpy!")
 time.sleep(.75)
-#os = input("What OS do you use? [Windows , Linux]:").strip().lower()
-#time.sleep(.4)
+
 print(f"{Fore.RED}1-Verifying Eligibity of your phone.{Style.RESET_ALL}\n"
       f"{Fore.GREEN}2-Setup Scrcpy!{Style.RESET_ALL}\n"
       f"{Fore.LIGHTBLUE_EX}3-Running it Wirelessly!{Style.RESET_ALL}\n"
