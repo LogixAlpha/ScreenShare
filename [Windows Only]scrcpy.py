90f61c2f29546bb20e075b4b5891e51b606bb184
LogixAlpha <ayoub.ouaddi.yt@gmail.com>
2026-09-12 21:50:56 +0100

    commit

--- [Windows Only]scrcpy.py
+++ [Windows Only]scrcpy.py
@@ -8,7 +8,7 @@ init(autoreset=True)
 
 print(f"{Back.CYAN}How to use Scrcpy!")
 time.sleep(.75)
-
+print(f"{Fore.LIGHTRED_EX}Make sure you ran this script on the scrcpy folder!{Style.RESET_ALL}\n")
 print(f"{Fore.RED}1-Verifying Eligibity of your phone.{Style.RESET_ALL}\n"
       f"{Fore.GREEN}2-Setup Scrcpy!{Style.RESET_ALL}\n"
       f"{Fore.LIGHTBLUE_EX}3-Running it Wirelessly!{Style.RESET_ALL}\n"
@@ -18,6 +18,7 @@ o = int(input(f"{Style.BRIGHT}Your option:{Style.RESET_ALL}"))
 if o == 1:
     time.sleep(.5)
     print(f"Running a script ...")
-    keyboard.press_and_release('CTRL+T')
-#else :
-#print("Not a root")
+    keyboard.press_and_release('WIN+R')
+    keyboard.write("cmd",0.5)
+    time.sleep(0.7)
+    
